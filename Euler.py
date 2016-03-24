841c16de79556ac9cff2f22c28c02cfaa04bb60c
Michal K <kuklewski.michal@gmail.com>
2016-03-24 13:54:05 +0100

    Eulerm - gotowy, zmienione indeksowanie w Euler i Dfs

--- Euler.py
+++ Euler.py
@@ -3,21 +3,19 @@
 from Graph import Graph
 
 def eulerianPath(graph):
-	# przy zmianie na numercję wierzchołków od 0 zmienić z j-1 na j
-	al = [ [j-1 for j in i] for i in graph.AL]
+	al = [ [j for j in i] for i in graph.AL]
 	print(al)
 	stack = []
 	i = 0
 	stack.append(i)
-	while al:
+	while al[0]:
 		j = al[i][0]
 		stack.append(j)
 		al[i].remove(j)
 		al[j].remove(i)
 		i = j
 		
-	for i in range(len(stack)):
-		print(stack[len(stack)-i], "->")
+	print(stack)
 
 		
 
